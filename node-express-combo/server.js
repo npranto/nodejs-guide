299a3a1766b3fd8beb6896fba7270634d25f80bb
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-05-15 15:38:47 -0400

    uses nodemailer to send emails to contact

--- node-express-combo/server.js
+++ node-express-combo/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {json, urlencoded} from 'body-parser';
 import path from 'path';
+import nodemailer from 'nodemailer';
 
 // declare port number
 const PORT = 3000;
@@ -36,6 +37,55 @@ app.get('/contact', (req, res) => {
     res.render('contact', {
         title: 'Contact'
     })
+})
+app.post('/contact/send', (req, res) => {
+    console.log(req.body);
+    console.log('Ready to send...');
+
+    // using nodemailer to send email
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nodeahmed001@gmail.com',
+            pass: 'abcde321'
+        }
+    })
+
+    // setup email data with unicode symbols
+    const mailOptions = {
+        from: 'Node Ahmed <nodeahmed001@gmail.com>',
+        to: 'npranto@gmail.com',
+        subject: 'Did NodeMailer Work?',
+        text: `Hey, you received an email from ${req.body.name} saying: \n Name: ${req.body.name} \n Email: ${req.body.email} \n Message: ${req.body.message} \n`,
+        html: `
+            <p> 
+                Hey,
+            </p>
+            <p> 
+                You received an email from <strong>${req.body.name}</strong> saying: 
+            </p>
+            <p>
+                Name: ${req.body.name}
+                <br />
+                Email: ${req.body.email}
+                <br />
+                Message: ${req.body.message}
+            </p>
+        `
+    }
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+        res.redirect('/');
+    })
+
+    console.log('Sent...');
 })
 
 // start listening to a port
