46358adc73d80c3d5efb4691ba077f771aa218f5
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-05-17 13:25:39 -0400

    adding the layout for register and login sections

--- node-auth/routes/users.js
+++ node-auth/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('login', {title: 'Login'});
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register', {title: 'Register'});
+});
+
 module.exports = router;
