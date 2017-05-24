88299283e493d404cdecd293613cf6edc2c37fc5
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-05-23 20:07:29 -0400

    setting up database and passport for local auth

--- local-auth/routes/index.js
+++ local-auth/routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var userRoutes = require('./users.js');
 
-var indexRoutes = function (app) {
+var indexRoutes = function (app, passport) {
 
     /* GET home page. */
     router.get('/', function(req, res, next) {
       res.render('index', { title: 'Express' });
     });
 
-    app.use('/users', userRoutes(app));
+    app.use('/users', userRoutes(app, passport));
 
     return router;
 
